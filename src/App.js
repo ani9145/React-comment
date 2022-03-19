2ab07f9e616836034ba874cf93fd3eeb68848a62
ani9145 <93372159+ani9145@users.noreply.github.com>
2022-03-19 22:43:16 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+import { useState } from "react";
+import { Comments } from './components/comment';
+import "./components/comment.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Comments />
+    </div>
+  );
+}
+
+export default App;
